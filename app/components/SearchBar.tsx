5e257d86cb9e67409451267cdfd6371bbdaa2423
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-12-10 15:52:50 +0530

    add some shadcn components

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -4,12 +4,12 @@ import { SearchIcon } from "lucide-react";
 
 const SearchBar = () => {
   return (
-    <div className="flex w-full max-w-xl mx-auto space-x-2 items-center justify-center">
+    <form className="flex w-full max-w-xl mx-auto space-x-2 items-center justify-center">
       <Input type="text" placeholder="Search products..." />
       <Button className="rounded-full px-3 bg-blue-500 hover:bg-blue-400">
         <SearchIcon className="w-5 h-5" />
       </Button>
-    </div>
+    </form>
   );
 };
 

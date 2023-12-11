5b7f46a29b191eaf30accdc878ee50a12ed1295f
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-12-11 16:29:53 +0530

    use fakeData json file to avoid api limit

--- app/search/[category]/page.tsx
+++ app/search/[category]/page.tsx
@@ -1,6 +1,6 @@
 import ImageSlider from "@/app/components/ImageSlider";
-import { products } from "@/constants";
 import Link from "next/link";
+import fakeData from "@/fakeData.json";
 
 export default async function Page({
   params,
@@ -10,39 +10,52 @@ export default async function Page({
   const { category } = params;
   const decodedCategory = decodeURIComponent(category);
 
-  // Fetch scrape data from API of OXYLABS using server actions
-  const username = "worldapp";
-  const password = "f#8X2m7Gr8!#kbB";
-  const body = {
-    source: "google_shopping_search",
-    domain: "com",
-    query: decodedCategory,
-    pages: 1,
-    parse: true,
-    geo_location: "India",
-    locale:"en-in",
-  };
-  const response = await fetch("https://realtime.oxylabs.io/v1/queries", {
-    method: "post",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
-    },
-    cache: "no-cache",
-  });
-  const data = await response.json();
-  console.log(data.results);
+  // Define a flag to determine whether to use fake data or real data
+  const useFakeData = true;
+
+  let data;
+  if (useFakeData) {
+    // Use fakeData to avoid API limit
+    console.log(fakeData);
+    data = fakeData;
+  } else {
+    // Use real data fetched from the API
+    const username = "worldapp";
+    const password = "f#8X2m7Gr8!#kbB";
+    const body = {
+      source: "google_shopping_search",
+      domain: "com",
+      query: decodedCategory,
+      pages: 1,
+      parse: true,
+      geo_location: "India",
+      locale: "en-in",
+    };
+
+    const response = await fetch("https://realtime.oxylabs.io/v1/queries", {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
+      },
+      cache: "no-cache",
+    });
+
+    data = await response.json();
+  }
 
   return (
     <>
       <h3 className="pb-5 font-light">Showing results for {decodedCategory}</h3>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-  {data.results[0].content?.results.organic?.map((product: any, i: number) => (
-    <ProductCard key={i} product={product} />
-  ))}
-</div>
+        {data.results[0]?.content?.results?.organic?.map(
+          (product: any, i: number) => (
+            <ProductCard key={i} product={product} />
+          )
+        )}
+      </div>
     </>
   );
 }

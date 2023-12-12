842bb0841aff9fde36d8a585cbbb77509b5021cf
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-12-12 21:45:45 +0530

    fix: loading skelton responsiveness

--- app/shopping/product/[id]/loading.tsx
+++ app/shopping/product/[id]/loading.tsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <>
       <div className="grid md:grid-cols-2 gap-4 grid-cols-1 md:space-x-4">
-        <Skeleton className="w-[500px] h-[350px] md:h-[500px]" />
+        <Skeleton className="w-86 lg:w-[500px] h-[350px] md:h-[500px]" />
         <div className="space-y-6">
           <Skeleton className="w-86 h-4" />
           <Skeleton className="w-56 h-4" />
@@ -16,7 +16,7 @@ export default function Loading() {
             <Skeleton className="w-66 h-4" />
           </div>
           <div className="space-y-3">
-            <Skeleton className="w-[450px] h-72" />
+            <Skeleton className="w-86 lg:w-[450px] h-72" />
           </div>
         </div>
       </div>
